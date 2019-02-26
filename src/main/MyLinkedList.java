fa9586912b83580a6b81f7e5523077dd03814fa7
Priyabanga <priya.banga_ccv17@gla.ac.in>
2019-02-26 02:40:27 -0800

    init commit

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -1,55 +1,55 @@
-package main;
-
-public class MyLinkedList {
-    private Node head;
-    private Node tail;
-    private int CurrentSize;
-
-    public Node getHead() {
-        return head;
-    }
-
-    public void setHead(Node head) {
-        this.head = head;
-    }
-
-    public Node getTail() {
-        return tail;
-    }
-
-    public void setTail(Node tail) {
-        this.tail = tail;
-    }
-
-    public int getCurrentSize() {
-        return CurrentSize;
-    }
-
-    public void setCurrentSize(int currentSize) {
-        CurrentSize = currentSize;
-    }
-
-    public MyLinkedList() {
-        this.head = null;
-        this.tail = null;
-        CurrentSize = 0;
-    }
-
-    public void displayAll()
-
-    public static void main(String[] args) {
-        MyLinkedList linkedList = new MyLinkedList();
-        Node node = new Node(10,null);
-
-        //linkedlist.insert(node)
-        /*if head --> null  AND tail --> null
-         * node-->next = null
-         * head = node
-         * tail = node*/
-
-        //linkedList.insert
-        //linkedList.remove
-    }
-
-
-}
+//package main;
+//
+//public class MyLinkedList {
+//    private Node head;
+//    private Node tail;
+//    private int CurrentSize;
+//
+//    public Node getHead() {
+//        return head;
+//    }
+//
+//    public void setHead(Node head) {
+//        this.head = head;
+//    }
+//
+//    public Node getTail() {
+//        return tail;
+//    }
+//
+//    public void setTail(Node tail) {
+//        this.tail = tail;
+//    }
+//
+//    public int getCurrentSize() {
+//        return CurrentSize;
+//    }
+//
+//    public void setCurrentSize(int currentSize) {
+//        CurrentSize = currentSize;
+//    }
+//
+//    public MyLinkedList() {
+//        this.head = null;
+//        this.tail = null;
+//        CurrentSize = 0;
+//    }
+//
+//    public void displayAll()
+//
+//    public static void main(String[] args) {
+//        MyLinkedList linkedList = new MyLinkedList();
+//        Node node = new Node(10,null);
+//
+//        //linkedlist.insert(node)
+//        /*if head --> null  AND tail --> null
+//         * node-->next = null
+//         * head = node
+//         * tail = node*/
+//
+//        //linkedList.insert
+//        //linkedList.remove
+//    }
+//
+//
+//}
---
+++ src/main/SinglyLinkedList.java
@@ -0,0 +1,51 @@
+package main;
+
+public class SinglyLinkedList {
+    private class Node{
+        private int data;
+        private Node next;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(Node next) {
+            this.next = next;
+        }
+    }
+    private Node head = null;
+     private int size = 0;
+
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        System.out.println(linkedList);
+    }
+    @Override
+    public String toString(){
+        StringBuilder response = new StringBuilder();
+        response.append("[");
+        response.append("]");
+        return response.toString();
+    }
+}
