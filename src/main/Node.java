4c691f70fa2a7796555ca5f99377632daa363b3d
Priyabanga <priya.banga_ccv17@gla.ac.in>
2019-02-26 02:23:14 -0800

    init commit

---
+++ src/main/Node.java
@@ -0,0 +1,40 @@
+package main;
+
+public class Node {
+    private int data;
+    private Node next;      //for singly linked list
+
+    // commit:ctrl+k
+    // push:shift+ctrl+k
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
